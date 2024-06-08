0978787eb81fcf84217555a7e6e9e0ed973ed769
Abylkairkhan <a_sarsetaev@kbtu.kz>
2024-06-09 01:55:47 +0500

    SignInScreen UI added

---
+++ app/src/main/java/kz/abyl/relogapp/presentation/sign_in/SignInScreen.kt
@@ -0,0 +1,235 @@
+package kz.abyl.relogapp.presentation.sign_in
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kz.abyl.relogapp.R
+
+@Composable
+fun SignInScreen(
+
+) {
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var passwordVisibility by remember { mutableStateOf(false) }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        Box(
+            modifier = Modifier
+                .weight(0.5f)
+        ) {
+            Image(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                painter = painterResource(id = R.drawable.rectangle_1),
+                contentDescription = "rectangle_1",
+                contentScale = ContentScale.Crop
+            )
+            Image(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                painter = painterResource(id = R.drawable.rectangle_2),
+                contentDescription = "rectangle_2",
+                contentScale = ContentScale.Crop
+            )
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(0.3f)
+                .padding(horizontal = 12.dp)
+        ) {
+            Text(
+                text = "Login",
+                fontFamily = FontFamily(Font(R.font.nunito_semibold)),
+                fontSize = 36.sp,
+                color = colorResource(id = R.color.blue)
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(60.dp),
+                value = username,
+                onValueChange = {
+                    username = it
+                },
+                singleLine = true,
+                label = {
+                    Text(
+                        text = "Email",
+                        color = colorResource(id = R.color.blue)
+                    )
+                },
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = colorResource(id = R.color.blue),
+                    unfocusedBorderColor = colorResource(id = R.color.blue),
+                ),
+                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done)
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(60.dp),
+                value = password,
+                onValueChange = {
+                    password = it
+                },
+                label = {
+                    Text(
+                        text = "Password",
+                        color = colorResource(id = R.color.blue)
+                    )
+                },
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = colorResource(id = R.color.blue),
+                    unfocusedBorderColor = colorResource(id = R.color.blue),
+                ),
+                visualTransformation = if (passwordVisibility) VisualTransformation.None else PasswordVisualTransformation(),
+                trailingIcon = {
+                    val visibilityIcon = if (passwordVisibility) painterResource(id = R.drawable.visibility_off_icon) else painterResource(id = R.drawable.visibility_icon)
+                    IconButton(
+                        onClick = { passwordVisibility = !passwordVisibility }
+                    ) {
+                        Icon(
+                            modifier = Modifier
+                                .size(32.dp),
+                            painter = visibilityIcon,
+                            contentDescription = "Password Visibility",
+                            tint = colorResource(id = R.color.blue)
+                        )
+                    }
+                }
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(
+                text = "Forgot Password?",
+                color = colorResource(id = R.color.blue),
+                modifier = Modifier.align(Alignment.End) // Align the text to the bottom end
+            )
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(0.20f)
+        ) {
+            Image(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                painter = painterResource(id = R.drawable.rectangle_3),
+                contentDescription = "rectangle_3",
+                contentScale = ContentScale.Crop
+            )
+            OutlinedButton(
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(16.dp),
+                shape = RoundedCornerShape(8.dp),
+                border = BorderStroke(1.dp, Color.White),
+                onClick = { /*TODO*/ }
+            ) {
+                Text(
+                    text = "Login",
+                    fontFamily = FontFamily(Font(R.font.nunito_semibold)),
+                    fontSize = 24.sp,
+                    color = Color.White
+                )
+            }
+            Row(
+                modifier = Modifier
+                    .align(Alignment.BottomStart)
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = "New Here?",
+                    fontFamily = FontFamily(Font(R.font.nunito_medium)),
+                    fontSize = 16.sp,
+                    color = Color.White
+                )
+                Spacer(modifier = Modifier.width(7.dp))
+                Text(
+                    modifier = Modifier
+                        .clickable {
+                            TODO()
+                        },
+                    text = "Register",
+                    fontFamily = FontFamily(Font(R.font.nunito_semibold)),
+                    fontSize = 16.sp,
+                    color = Color.White
+                )
+            }
+            Card(
+                modifier = Modifier
+                    .padding(horizontal = 12.dp)
+                    .size(45.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color.White
+                ),
+                elevation = CardDefaults.cardElevation(
+                    defaultElevation = 10.dp
+                )
+            ) {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center // Center the content
+                ) {
+                    Image(
+                        modifier = Modifier.size(30.dp),
+                        painter = painterResource(id = R.drawable.google_icon),
+                        contentDescription = "google_icon"
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SignInScreenPreview() {
+    SignInScreen()
+}
